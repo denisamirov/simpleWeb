36d3309dbe393b67b33e240113c5f098ea15abc1
denis <amirowdenis@yandex.ru>
2023-03-03 18:24:05 +0300

    dino-new

--- html/games/dino/dino.js
+++ html/games/dino/dino.js
@@ -1,34 +1,57 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const game = document.getElementById("game");
+
 
 //Чтобы игра не запускалась сразу
 cactus.classList.remove("cactus")
 //Чтобы игра не запускалась сразу
 
+//При нажатии на любую клавишу будет выполняться функция jump()
 document.addEventListener("keydown", function(event) {
     jump();
 });
 
-
+//Определение функции jump()
 function jump () {
+    //Если у динозавра нет класса jump добавить ему этот класс
     if (dino.classList != "jump") {
         dino.classList.add("jump")
     }
-
+    //По истечению 300 мс у динозаврика удалить класс jump
     setTimeout(function() {
         dino.classList.remove("jump")
-    }, 300)
+    }, 600)
 }
 
 
 let isAlive = setInterval (function() {
+    let incr = document.getElementById('numb').value;
+    let lose = document.getElementById('numb2').value;  //Новое
+    //Старт.... Получаем положение динозаврика и кактуса
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
-
+    //Конец... Получаем положение динозаврика и кактуса
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("GameOVER")
+        alert("GameOVER");
+        document.getElementById('numb2').value = Number(lose) + 1   //Новое
+        document.getElementById('numb').value = 0;
+        incr = 0;
+        gameOvers(lose);
     }
-}, 10)
+
+    if (incr > 500 && incr < 2000) {
+        game.classList.add("game2")
+        game.classList.remove("game")
+    }
+    else if (incr > 3000) {
+        game.classList.add("game3")
+        game.classList.remove("game2")
+
+    }
+    
+    document.getElementById('numb').value = Number(incr) + 1
+                                        }, 10)
 
 
 document.addEventListener('keydown', function(event) {
@@ -40,4 +63,38 @@ document.addEventListener('keydown', function(event) {
         dino.classList.add("jump")
         cactus.classList.add("cactus")
     }
+
   });
+
+
+let legs  = setInterval(function() {
+
+    setTimeout(function() {
+        dino.classList.remove("dino")
+        dino.classList.add("dino2")
+    }, 500)
+
+    setTimeout(function() {
+        dino.classList.remove("dino2")
+        dino.classList.add("dino")
+    }, 600)
+  
+}, 200)
+
+
+//Новая функция 
+function gameOvers(incrLose) {
+    const life1 = document.getElementById("life1")
+    const life2 = document.getElementById("life2")
+    const life3 = document.getElementById("life3")
+    if (incrLose > 1) {
+        life1.classList.add("non-life")
+    }
+    if (incrLose > 2 && incrLose < 5) {
+        life2.classList.add("non-life")
+    }
+    if (incrLose > 4 && incrLose < 6) {
+        life3.classList.add("non-life")
+    }
+} 
+
--- js/lk.js
+++ js/lk.js
@@ -1,6 +1,5 @@
 let a = prompt("Введите пароль");
 
-
 if (a == '123') {
     alert('Congratulation!')
 }
@@ -9,3 +8,22 @@ else {
     //Редирект на страницу, расположенную по адресу /html/
     document.location.href = '/html/'
 }
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == 'KeyX') {
+        diva.classList.add("second")
+        diva.classList.remove("first")
+    }
+
+    if (event.code == 'KeyZ') {
+        diva.classList.add("first")
+        diva.classList.remove("second")
+    }
+});
+
+
+const diva = document.getElementById("diva")
+console.log(diva.classList)
+
+
